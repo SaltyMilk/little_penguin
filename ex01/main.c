1d66e55fe4fb8482fbfc238220f81b6248a0c157
SaltyMilk <soufiane.elmelcaoui@gmail.com>
2022-07-02 04:34:26 +0200

    first draft for ex01, might need to change to get correct date

---
+++ ex01/main.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_AUTHOR("531-M31C");
+MODULE_LICENSE("GPL");
+
+
+static int __init main_init(void)
+{
+	printk(KERN_INFO "Hello world !\n");
+	return 0;
+}
+
+static void __exit main_clean(void)
+{
+	printk(KERN_INFO "Cleaning up module.\n");
+}
+module_init(main_init);
+module_exit(main_clean);
