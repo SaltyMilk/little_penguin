7b308a6b01b972a5675dc34da49f11e786caec36
SaltyMilk <soufiane.elmelcaoui@gmail.com>
2022-07-12 06:55:40 +0200

    ex07 id file

---
+++ ex07/main.c
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: GPL-2.0+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/device.h>
+#include <linux/usb.h>
+#include <linux/mod_devicetable.h>
+#include <linux/miscdevice.h>
+#include <linux/uaccess.h>
+#include <linux/errno.h>
+#include <linux/debugfs.h>
+
+MODULE_AUTHOR("IROH");
+MODULE_DESCRIPTION("A simple login module");
+MODULE_LICENSE("GPL");
+
+#define INTRA_LOGIN "hsmits"
+
+static struct dentry *dfs_dir;
+static struct dentry *id_file;
+
+static ssize_t id_read(struct file *file, char __user *buff, size_t size, loff_t *_offset)
+{
+	return simple_read_from_buffer(buff, size, _offset, INTRA_LOGIN, sizeof(INTRA_LOGIN) - 1);
+}
+
+static ssize_t id_write(struct file *file, const char __user *buff, size_t size, loff_t *_offset)
+{
+	if (size != sizeof(INTRA_LOGIN) - 1)
+		return -EINVAL;
+	if (memcmp(buff, INTRA_LOGIN, sizeof(INTRA_LOGIN) - 1))
+		return -EINVAL;
+	return sizeof(INTRA_LOGIN) - 1;
+}
+
+static struct file_operations id_fops = {
+
+	.read = &id_read,
+	.write = &id_write,
+};
+
+
+static int __init init(void)
+{
+	printk(KERN_INFO "Module init.\n");
+
+	dfs_dir = debugfs_create_dir("fortytwo", NULL);
+	id_file = debugfs_create_file("id", 0777, dfs_dir, NULL, &id_fops);
+	return (0);
+
+}
+
+static void __exit cleanup(void)
+{
+	printk(KERN_INFO "Module cleanup.\n");
+	debugfs_remove(id_file);
+	debugfs_remove(dfs_dir);
+}
+
+
+module_init(init);
+module_exit(cleanup);
