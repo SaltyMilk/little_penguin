92b70b79f4759d93f39dce7dda09ea0f19fcff8e
SaltyMilk <soufiane.elmelcaoui@gmail.com>
2022-07-12 07:28:00 +0200

    ex 07 jiffies

--- ex07/main.c
+++ ex07/main.c
@@ -18,10 +18,20 @@ MODULE_LICENSE("GPL");
 
 static struct dentry *dfs_dir;
 static struct dentry *id_file;
+static struct dentry *jiffies_file;
 
 static ssize_t id_read(struct file *file, char __user *buff, size_t size, loff_t *_offset)
 {
 	return simple_read_from_buffer(buff, size, _offset, INTRA_LOGIN, sizeof(INTRA_LOGIN) - 1);
+}
+
+static ssize_t jiffies_read(struct file *file, char __user *buff, size_t size, loff_t *_offset)
+{
+	char buffer[64];
+
+	memset(buffer, 0, 64);
+	snprintf(buffer, 64, "%lu", jiffies);
+	return (simple_read_from_buffer(buff, size, _offset, buffer, 64));
 }
 
 static ssize_t id_write(struct file *file, const char __user *buff, size_t size, loff_t *_offset)
@@ -39,6 +49,10 @@ static struct file_operations id_fops = {
 	.write = &id_write,
 };
 
+static struct file_operations jiffies_fops = {
+
+	.read = &jiffies_read,
+};
 
 static int __init init(void)
 {
@@ -46,6 +60,7 @@ static int __init init(void)
 
 	dfs_dir = debugfs_create_dir("fortytwo", NULL);
 	id_file = debugfs_create_file("id", 0777, dfs_dir, NULL, &id_fops);
+	jiffies_file = debugfs_create_file("jiffies", 0444, dfs_dir, NULL, &jiffies_fops);
 	return (0);
 
 }
@@ -54,6 +69,7 @@ static void __exit cleanup(void)
 {
 	printk(KERN_INFO "Module cleanup.\n");
 	debugfs_remove(id_file);
+	debugfs_remove(jiffies_file);
 	debugfs_remove(dfs_dir);
 }
 
